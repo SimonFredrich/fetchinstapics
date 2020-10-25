096c727db184d316849b38e420b0b45ef72b5263
SimonFredrich <21simplex02@gmail.com>
2020-10-25 18:37:48 +0100

    geckodriver installation script

---
+++ install_geckodriver.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+wget https://github.com/mozilla/geckodriver/releases/download/v0.27.0/geckodriver-v0.27.0-linux32.tar.gz
+tar -xvzf geckodriver-v0.27.0-linux32.tar.gz -C /usr/local/bin
+rm -rf geckodriver-v0.27.0-linux32.tar.gz
