89baf0f6315936be64789c1a22a355ac77574d41
SimonFredrich <21simplex02@gmail.com>
2020-10-31 23:39:22 +0100

    working for entire pages (adaptive scrolling)

--- main.py
+++ main.py
@@ -5,30 +5,29 @@ import urllib.request
 import time
 
 # https://dev.to/mr_h/python-selenium-infinite-scrolling-3o12
-def scroll(driver):
+def scroll(driver, links):
     # scroll_pause_time = 0
 
     last_urls = driver.find_elements_by_class_name("FFVAD")
-    new_urls = []
+    last_url = last_urls[-1].get_attribute("src")
     
-    print("holla")
-    print(driver.execute_script("return document.body.scrollHeight"))
-
     while True:
         # Scroll down to bottom
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        # # Wait to load page
-        # time.sleep(scroll_pause_time)
         time.sleep(5)
 
-        print("hlow")
-        print(driver.execute_script("return document.body.scrollHeight"))
+        new_urls = driver.find_elements_by_class_name("FFVAD")
+        for i in new_urls:
+            link = i.get_attribute("src")
+            if link not in links:
+                links.append(link)
 
-        if (len(last_urls) == len(new_urls)):
+        new_url = new_urls[-1].get_attribute("src")
+
+        if last_url == new_url:
             break
-
-        last_urls = new_urls
+        
+        last_url = new_url
 
 
 if __name__ == "__main__":
@@ -41,13 +40,16 @@ if __name__ == "__main__":
 
     driver = webdriver.Firefox()
     driver.get(url)
-    scroll(driver)
-    elements = driver.find_elements_by_class_name("FFVAD")
+
+    links = []
+    scroll(driver, links)
+    print(links)
+    # elements = driver.find_elements_by_class_name("FFVAD")
+    # print(len(elements))
 
     img_counter = 2
-    for item in elements:
-        pic_url = item.get_attribute("src")
-        r = urllib.request.urlopen(pic_url)
+    for link in links:
+        r = urllib.request.urlopen(link)
         with open("./pics/" + str(img_counter) + ".jpg", "wb") as f:
             f.write(r.read())
         img_counter = img_counter + 1
