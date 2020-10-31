a9df60e27f89944ac7e4130ae6b09f1004e03d85
SimonFredrich <21simplex02@gmail.com>
2020-10-31 23:57:45 +0100

    clean code and write comments

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from selenium.webdriver.firefox.options import Options
 import urllib.request
 import time
 
-# https://dev.to/mr_h/python-selenium-infinite-scrolling-3o12
 def scroll(driver, links):
-    # scroll_pause_time = 0
 
+    # fetch all images visible
     last_urls = driver.find_elements_by_class_name("FFVAD")
+    # save url from last image fetch
     last_url = last_urls[-1].get_attribute("src")
     
     while True:
@@ -16,37 +16,47 @@ def scroll(driver, links):
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(5)
 
+        # fetch all images visible (if new loaded then they will be also saved)
         new_urls = driver.find_elements_by_class_name("FFVAD")
+        # save image urls in "links" list
         for i in new_urls:
             link = i.get_attribute("src")
             if link not in links:
                 links.append(link)
 
+        # save url from new image fetch
         new_url = new_urls[-1].get_attribute("src")
 
+        # compare last and new url to know if we are at the bottom of the
+        # instagram profile page
         if last_url == new_url:
             break
         
+        # catch up the last url
         last_url = new_url
 
 
 if __name__ == "__main__":
+    # get username and with that the url to the insta page
     uname = input("Username of instagram page: ")
     url = "https://www.instagram.com/" + uname
 
+    # set a few options
     options = Options()
     options.set_preference('permissions.default.image', 2)
     options.set_preference('dom.ipc.plugins.enabled.libflashplayer.so', False)
 
+    # setup the firefox driver
     driver = webdriver.Firefox()
+    # navigate to url
     driver.get(url)
 
+    # declare link list
     links = []
+    # scroll through the whole instagram profile page
     scroll(driver, links)
-    print(links)
-    # elements = driver.find_elements_by_class_name("FFVAD")
-    # print(len(elements))
 
+    # go through links, download and save images
     img_counter = 2
     for link in links:
         r = urllib.request.urlopen(link)
@@ -54,4 +64,5 @@ if __name__ == "__main__":
             f.write(r.read())
         img_counter = img_counter + 1
 
+    # close Firefox driver
     driver.close()
